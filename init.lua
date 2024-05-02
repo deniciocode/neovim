e68c249e17ec23d0d985361de42eab7b147e52af
Dennis Haegler <dennis.haegler@gmail.com>
2024-05-02 11:06:09 +0200

    Update config
    Upgrade plugins
    
    Upgrade plugins

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Keep signcolumn on by default
+vim.wo.signcolumn = "yes"
+vim.opt.colorcolumn = "81"
+vim.o.relativenumber = false
+
+vim.cmd("colorscheme everforest")
