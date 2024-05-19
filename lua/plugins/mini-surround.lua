8cba61d5f04eaf2c241e8201b03d8d63318b5828
Dennis Haegler <dennis.haegler@gmail.com>
2024-05-19 13:14:20 +0200

    WIP add mappings for surround

---
+++ lua/plugins/mini-surround.lua
@@ -0,0 +1,13 @@
+return {
+  "echasnovski/mini.surround",
+  opts = {
+    mappings = {
+      add = "gsa",
+      delete = "gsd",
+      find = "gsf",
+      find_left = "gsF",
+      highlight = "gsh",
+      update_n_lines = "gsn",
+    },
+  },
+}
