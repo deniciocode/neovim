330c97c167358bfc5dd02703d21a3af60ff69002
Dennis Haegler <dennis.haegler@gmail.com>
2024-11-20 18:07:38 +0100

    Add keymaps for vim-rails

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -20,3 +20,54 @@ require("telescope").setup({
     },
   },
 })
+
+vim.keymap.set(
+  "n",
+  "<leader>ov",
+  ":Eview ",
+  { noremap = true, silent = false, desc = "Edit Rails view" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>oc",
+  ":Econtroller ",
+  { noremap = true, silent = false, desc = "Edit Rails controller" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>om",
+  ":Emodel ",
+  { noremap = true, silent = false, desc = "Edit Rails model" }
+)
+
+-- Open Rails helper
+vim.keymap.set(
+  "n",
+  "<leader>oh",
+  ":Ehelper ",
+  { noremap = true, silent = false, desc = "Edit Rails helper" }
+)
+
+-- Open Rails migration
+vim.keymap.set(
+  "n",
+  "<leader>og",
+  ":Emigration ",
+  { noremap = true, silent = false, desc = "Edit Rails migration" }
+)
+
+-- Open Rails spec (RSpec file)
+vim.keymap.set(
+  "n",
+  "<leader>ot",
+  ":Espec ",
+  { noremap = true, silent = false, desc = "Edit Rails spec file" }
+)
+
+-- Open Rails schema
+vim.keymap.set(
+  "n",
+  "<leader>os",
+  ":Eschema ",
+  { noremap = true, silent = false, desc = "View Rails schema" }
+)
