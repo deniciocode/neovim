92c6dc6bcb6350dbf0a7fb900fa88d476e04f3c3
Dennis Haegler <dennis.haegler@gmail.com>
2024-12-20 22:08:03 +0100

    Remove custom mapping for erb formatting

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,13 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
 local actions = require("telescope.actions")
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>cn",
-  ":!erb-format --write %<CR>",
-  { noremap = true, silent = true }
-)
 require("telescope").setup({
   defaults = {
     mappings = {
