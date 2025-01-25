1c606fb4544e654bd955c5bb9902dfd9bc1c1aed
Dennis Haegler <dennis.haegler@gmail.com>
2025-01-25 08:03:05 +0100

    Remove old telescope config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,20 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local actions = require("telescope.actions")
-require("telescope").setup({
-  defaults = {
-    mappings = {
-      i = {
-        ["<C-n>"] = actions.cycle_history_next,
-        ["<C-p>"] = actions.cycle_history_prev,
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
-      },
-    },
-  },
-})
-
 vim.keymap.set(
   "n",
   "<leader>ov",
