52314e5b412910f181e9bcd5205386e141887b3a
Dennis Haegler <dennis.haegler@gmail.com>
2025-09-17 23:11:25 +0200

    Fix mason and mason lsp

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,11 +1,8 @@
 return {
   {
     "mason-org/mason.nvim",
-    version = "1.11.0",
     opts = {
       ensure_installed = { "ruby-lsp", "rubocop", "erb-formatter" },
     },
   },
-
-  { "mason-org/mason-lspconfig.nvim", version = "1.32.0" },
 }
