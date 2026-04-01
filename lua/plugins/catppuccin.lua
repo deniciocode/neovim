70e636fa8bd3204d8e65168ec3d1878dfbfa7143
Dennis Haegler <dennis.haegler@gmail.com>
2026-04-01 21:17:41 +0200

    Cleanup colorscheme files

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,12 +1,13 @@
 return {
   "catppuccin/nvim",
   name = "catppuccin",
+  lazy = false,
   priority = 1000, -- make sure it loads first
   config = function()
     require("catppuccin").setup({
       background = {
         light = "latte",
-        dark = "frappe", -- override default dark flavour
+        dark = "frappe",
       },
     })
   end,
--- lua/plugins/everforest.lua
+++ lua/plugins/everforest.lua
@@ -1,22 +1,22 @@
 return {
-  "neanias/everforest-nvim",
-  version = false,
-  lazy = false,
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    local is_dark = vim.fn
-      .system("defaults read -g AppleInterfaceStyle 2>/dev/null")
-      :match("Dark") ~= nil
-    vim.o.background = is_dark and "dark" or "light"
+  {
+    "neanias/everforest-nvim",
+    version = false,
+    lazy = false,
+    enabled = true,
+    priority = 1000,
+    config = function()
+      local is_dark = vim.fn
+        .system("defaults read -g AppleInterfaceStyle 2>/dev/null")
+        :match("Dark") ~= nil
+      vim.o.background = is_dark and "dark" or "light"
 
-    require("everforest").setup({
-      on_highlights = function(hl, palette)
-        -- hl.TSSymbol = { link = "Yellow" } -- default is Agua
-        -- hl.TSBoolean = { fg = palette.purple, bg = palette.none, bold = true }
-        hl.TSParameter = { link = palette.yellow }
-        hl.TSParameter = { link = "Yellow" }
-      end,
-    })
-    vim.cmd.colorscheme("everforest")
-  end,
+      require("everforest").setup({
+        on_highlights = function(highlight, palette)
+          highlight.TSParameter = { fg = palette.yellow, bg = palette.none }
+        end,
+      })
+      vim.cmd.colorscheme("everforest")
+    end,
+  },
 }
