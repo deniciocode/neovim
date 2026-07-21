f2ee08a9821067c985bd2b64800c6b3ccf63fd20
Dennis Haegler <dennis.haegler@gmail.com>
2026-07-21 15:59:02 +0200

    Support mjml highlighting

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,3 +7,12 @@ vim.opt.colorcolumn = "81"
 vim.opt.relativenumber = false
 vim.opt.fillchars:append({ eob = " " })
 vim.opt.textwidth = 80
+
+vim.filetype.add({
+  extension = {
+    mjml = "mjml",
+  },
+})
+
+-- MJML has no dedicated treesitter grammar; use the html parser
+vim.treesitter.language.register("html", "mjml")
